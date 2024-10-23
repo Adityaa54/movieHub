895410cfe9f2163ac805159b545836ab9801869c
Pooja kadam <kadampoo431@gmail.com>
2024-10-23 12:57:31 +0530

    second

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 function Navbar() {
   return (
     <>
-      <div className="flex items-center justify-between border-b border-green-50 bg-black px-6 py-2 md:px-12">
+      <div className="flex flex-col md:flex-row items-center justify-between border-b border-green-50 bg-black px-6 py-2 md:px-12 space-y-2 md:space-y-0">
         {/* Logo */}
         <div className="flex items-center space-x-2">
           <div className="w-12 h-12 sm:w-16 sm:h-16">
@@ -13,34 +14,32 @@ function Navbar() {
               className="h-full w-full rounded-full object-cover"
             />
           </div>
-          <div className="hidden text-white font-bold sm:block">
+          <div className="text-white font-bold sm:block">
             <span className="text-lg md:text-xl">MovieDB</span>
           </div>
         </div>
 
         {/* Links and Search Bar */}
-        <div className="flex items-center space-x-4">
+        <div className="flex flex-col md:flex-row items-center space-y-2 md:space-y-0 md:space-x-4 w-full md:w-auto">
           {/* Links */}
-          <div className="hidden md:flex items-center space-x-4">
-            <Link to="/" className='text-white hover:text-pink-600'>Popular</Link>
-            <Link to="/toprated" className='text-white hover:text-pink-600'>Top Rated</Link>
-            <Link to="/upcoming" className='text-white hover:text-pink-600'>Up Coming</Link>
+          <div className="flex justify-center space-x-4">
+            <Link to="/" className="text-white hover:text-pink-600">Popular</Link>
+            <Link to="/toprated" className="text-white hover:text-pink-600">Top Rated</Link>
+            <Link to="/upcoming" className="text-white hover:text-pink-600">Upcoming</Link>
           </div>
 
           {/* Search Bar */}
-          <div className="flex items-center space-x-2">
+          <div className="flex items-center space-x-2 w-full justify-center md:w-auto">
             <input
               type="text"
               placeholder="Movie Name"
-              className="bg-gray-800 text-white text-sm px-3 py-1 rounded w-32 md:w-64 focus:outline-none"
+              className="bg-gray-800 text-white text-sm px-3 py-1 rounded w-full md:w-64 focus:outline-none"
             />
             <button className="bg-blue-600 text-white px-4 py-1 rounded hover:bg-blue-700" aria-label="Search Movies">
               Search
             </button>
           </div>
         </div>
-
-        
       </div>
     </>
   );
